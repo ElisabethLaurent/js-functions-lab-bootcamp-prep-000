4ed4aa0348d9a479c4122e3f59db82fe4c48e00c
Elisabeth L <elisabethsochacki@gmail.com>
2017-10-10 14:41:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code below!
+
+function happyHolidays () {
+  return"Happy holidays!"
+};
+var name = you
+function happyHolidaysTo(name) {
+  return("Happy holidays, ${name}!")
+};
+happyHolidaysTo("Sam");
